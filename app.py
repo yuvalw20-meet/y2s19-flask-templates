2c8dcf1d77529d709bfc9e414e043b12217380d2
myname21-meet <myname21@meet.mit.edu>
2019-07-30 03:34:43 -0400

    updated lab

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+	oppiboii = False
+	foodslist = ["Steak", "Burger", "Chibito", "Schnitzel"]
+	hatedefs = ["Tofu", "Cauliflower Chickpea Patties","Zucchini and Tomato Lasagna With Cashew Herb Cheese"]
+	return render_template("index.html",listboii = foodslist,oppoday = oppiboii, oppolist = hatedefs)
 
 if __name__ == '__main__':
    app.run(debug = True)
